e19384c7c0ca8fadf709beb96297255a450e7200
w <none>
2016-09-29 03:22:01 -0400

    version etc

--- app.py
+++ app.py
@@ -3,10 +3,11 @@ import os
 
 import flask
 import youtube_dl
+import youtube_dl.version
 
+ydl_ver = youtube_dl.version.__version__
 app = flask.Flask(__name__)
 
-ydl = youtube_dl.YoutubeDL()
 destination = 'http://zippy.gfycat.com/HalfSmugAmericancicada.mp4'
 
 @app.route('/')
@@ -32,11 +33,12 @@ def extract():
 	if not location or ':' not in location:
 		flask.abort(400)
 	try:
+		ydl = youtube_dl.YoutubeDL(params={'no_color': True})
 		result = ydl.extract_info(location, download=False)
 	except Exception, e:
 		app.log_exception(e)
-		return flask.render_template('not-extracted.html', formatted_exc=str(e))
-	return flask.render_template('extracted.html', result=result, formatted_result=json.dumps(result, indent=2))
+		return flask.render_template('not-extracted.html', version=ydl_ver, formatted_exc=str(e)), 500
+	return flask.render_template('extracted.html', version=ydl_ver, result=result, formatted_result=json.dumps(result, indent=2))
 
 port = int(os.environ.get('PORT', '5000'))
 app.run(host='0.0.0.0', port=port)
